7ad7dcf76827eb6f787850cc03a3840e5fd01975
Fede Montagud <fedemontagudo@gmail.com>
2021-03-13 14:31:55 +0100

    obtencion del objeto con las facturas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
 import useFetch from "./Hooks/useFetch";
-console.log(useFetch);
+
 function App() {
   const { datos, cargando } = useFetch(`${process.env.REACT_APP_API_URL}`);
+  const [total, setTotal] = useState(0);
   useEffect(() => {
     console.log(datos);
   }, [datos]);
   return (
     <>
-      <h1>resultados</h1>
+      <h1>resultados </h1>
     </>
   );
 }
