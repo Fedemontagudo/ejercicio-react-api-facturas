31720959f1e5cb2398a57a3fff6b695246af31df
Fede Montagud <fedemontagudo@gmail.com>
2021-03-13 12:40:12 +0100

    copiado custom hook y probado en app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-
+import { useEffect, useState } from "react";
+import useFetch from "./Hooks/useFetch";
+console.log(useFetch);
 function App() {
+  const { datos, cargando } = useFetch(`${process.env.REACT_APP_API_URL}`);
+  useEffect(() => {
+    console.log(datos);
+  }, [datos]);
   return (
     <>
+      <h1>resultados</h1>
     </>
   );
 }
