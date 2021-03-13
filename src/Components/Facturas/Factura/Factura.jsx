2eef75a01baef62c02cb2b6fa5c4c52ca5989a83
Miquel Castro <castro-miquel@hotmail.com>
2021-03-14 00:22:03 +0100

    Añado las facturas a la tabla falta el vencimento

---
+++ src/Components/Facturas/Factura/Factura.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { DateTime } from "luxon";
+
+const Factura = (props) => {
+  const { id, numero, fecha, vencimiento, concepto, base, tipoIva, abonada } = props.factura;
+  console.log(props);
+  return (
+    <>
+      <tr>
+        <td>{id}</td>
+        <td>{DateTime.fromMillis(parseInt(fecha)).setLocale("es").toLocaleString()}</td>
+        <td>{concepto}</td>
+        <td>{base} €</td>
+        <td>{`${base * (1 + tipoIva) / 100} €`}</td>
+        <td>{`${(base * (1 + tipoIva) / 100) + base} €`}</td>
+        <td className={abonada ? "table-success" : "table-danger"}>{abonada ? "abonada" : "pendiente"}</td>
+        <td>
+          {`${DateTime.fromMillis(parseInt(vencimiento)).setLocale("es").toLocaleString()}
+          faltan ${DateTime.now().diff(DateTime.fromMillis(parseInt(vencimiento)), "days")}
+          ${DateTime.now().setLocale("es").toLocaleString()}
+          `
+          }
+
+        </td>
+      </tr>
+    </>
+  );
+};
+
+export default Factura;
